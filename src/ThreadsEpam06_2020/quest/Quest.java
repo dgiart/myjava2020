7a8ce1e676fbdc885e4300f8def391f7775d0056
zlfqDTwy <zlfqdtwy@email.com>
2020-08-12 16:41:20 +0300

    quest in process +

--- src/ThreadsEpam06_2020/quest/Quest.java
+++ src/ThreadsEpam06_2020/quest/Quest.java
@@ -19,6 +19,15 @@ class Rem extends Thread{
                 Quest.flag = false;
                 currentThread().interrupt();
             }
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println("By-By!");
+                Quest.flag = false;
+                return;
+            } catch (InterruptedException e) {
+                Quest.flag = false;
+                currentThread().interrupt();
+            }
         }
         Quest.flag = false;
     }
@@ -30,21 +39,29 @@ public class Quest {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             System.out.println(flag);
-            flag = true;
+//            flag = true;
+            int count = 0;
             while (flag == true){
+                count++;
+                System.out.println("count = " + count);
                 System.out.println(questions[i]);
                 Rem rem1=new Rem(10,"Are you here?");
                 rem1.start();
                 Scanner scanner=new Scanner(System.in);
                 String answer=scanner.nextLine();
-                if(answer.equals("stop")) return;
+                if(answer.equals("stop")) {
+                    rem1.interrupt();
+                    flag = false;
+                    break;
+                }
                 if(answer.equals(answers[i])){
                     System.out.println("Correct");
                     rem1.interrupt();
+                    flag = true;
                     break;
                 }
                 else {
-                    System.out.println("You wrote " + answer);
+                    System.out.println("Your answer: " + answer + " is WRONG");
                 }
             }
         }
